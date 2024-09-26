0ae73f0f3fad1d252f1eb72e6a31308f4ea8c393
jstrmby <48553432+jstmrby@users.noreply.github.com>
2024-09-26 19:32:28 +0200

    change font to intel one

--- font.lua
+++ font.lua
@@ -4,9 +4,9 @@ local module = {}
 
 function module.set_font(config)
 	config.color_scheme = "Obsidian (Gogh)"
-	config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Medium" })
+	config.font = wezterm.font("IntoneMono Nerd Font Mono", { weight = "Medium" })
 	config.font_size = 14.0
-	config.line_height = 1.125
+	config.line_height = 1.05
 
 	config.window_frame = {
 		font = wezterm.font("Hurmit Nerd Font Mono", { weight = "Bold" }),
