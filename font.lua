8eb7826a7bf428eb2b18f6bfc20f8de2e69a107a
jstrmby <48553432+jstmrby@users.noreply.github.com>
2024-10-25 12:05:31 +0200

    increase line height

--- font.lua
+++ font.lua
@@ -6,7 +6,7 @@ function module.set_font(config)
 	config.color_scheme = "Obsidian (Gogh)"
 	config.font = wezterm.font("IntoneMono Nerd Font Mono", { weight = "Medium" })
 	config.font_size = 14.0
-	config.line_height = 1.05
+	config.line_height = 1.1
 
 	config.window_frame = {
 		font = wezterm.font("Hurmit Nerd Font Mono", { weight = "Bold" }),
